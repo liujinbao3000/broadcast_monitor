5a9d98052270cd56a1ba4962ffb47b2bd061db3a
amogus <liujinbao3000@163.com>
2024-09-26 17:52:33 +0800

    Update broadcast_monitor.go

--- broadcast_monitor.go
+++ broadcast_monitor.go
@@ -1,92 +1,122 @@
 package main
 
 import (
-    "bytes"
     "fmt"
+    "log"
     "os"
     "os/signal"
+    "syscall"
     "time"
+
     "github.com/google/gopacket"
     "github.com/google/gopacket/layers"
     "github.com/google/gopacket/pcap"
-    "net"
-    "syscall"
 )
 
+const updateInterval = 5 * time.Second
+
 func main() {
-    // 获取所有网卡
-    devices, err := pcap.FindAllDevs()
+    // 获取所有网络接口
+    interfaces, err := pcap.FindAllDevs()
     if err != nil {
-        fmt.Printf("Error finding devices: %v\n", err)
-        return
+        log.Fatal(err)
     }
 
-    // 列出所有网卡供用户选择
-    fmt.Println("可用的网络接口：")
-    for i, device := range devices {
-        fmt.Printf("[%d] %s (%s)\n", i, device.Name, device.Description)
+    // 打印可用的网络接口
+    fmt.Println("可用的网络接口:")
+    for i, iface := range interfaces {
+        ipv4 := getIPv4(iface)
+        fmt.Printf("%d. %s - %s\n", i+1, iface.Description, ipv4)
     }
 
-    // 选择网卡
+    // 让用户选择网络接口
     var choice int
-    fmt.Print("请选择要监控的网卡编号：")
+    fmt.Print("请选择要监控的网络接口 (输入数字): ")
     _, err = fmt.Scan(&choice)
-    if err != nil || choice < 0 || choice >= len(devices) {
-        fmt.Println("无效的选择")
-        return
+    if err != nil || choice < 1 || choice > len(interfaces) {
+        log.Fatal("无效的选择")
     }
-    device := devices[choice].Name
-    fmt.Printf("监控网卡: %s\n", device)
 
-    // 打开网卡进行监听
-    handle, err := pcap.OpenLive(device, 1600, true, pcap.BlockForever)
+    interfaceName := interfaces[choice-1].Name
+
+    // 打开网络接口
+    handle, err := pcap.OpenLive(interfaceName, 1600, true, pcap.BlockForever)
     if err != nil {
-        fmt.Printf("Error opening device %s: %v\n", device, err)
-        return
+        log.Fatal(err)
     }
     defer handle.Close()
 
-    // 设置信号监听，捕捉Ctrl+C来安全退出
-    sigChan := make(chan os.Signal, 1)
-    done := make(chan bool)
-    signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+    // 设置过滤器,只捕获广播包
+    err = handle.SetBPFFilter("ether broadcast")
+    if err != nil {
+        log.Fatal(err)
+    }
 
+    // 初始化计数器和时间
+    broadcastCount := 0
+    startTime := time.Now()
+
+    // 创建数据包源
     packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
-    packets := packetSource.Packets()
 
-    // 启动广播包统计
+    fmt.Printf("开始监控接口 %s 上的广播包...\n", interfaceName)
+    fmt.Printf("统计信息每 %d 秒更新一次\n", int(updateInterval.Seconds()))
+    fmt.Println("按 Ctrl+C 可以退出程序")
+
+    // 设置信号处理
+    signalChan := make(chan os.Signal, 1)
+    signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
+
+    // 创建一个通道用于停止抓包
+    stop := make(chan struct{})
+
     go func() {
-        ticker := time.NewTicker(1 * time.Second)
-        defer ticker.Stop()
-
-        broadcastPackets := 0
-        for {
-            select {
-            case packet := <-packets:
-                if packet == nil {
-                    return // 安全退出goroutine
-                }
-                // 判断是否是广播包
-                ethLayer := packet.Layer(layers.LayerTypeEthernet)
-                if ethLayer != nil {
-                    eth, _ := ethLayer.(*layers.Ethernet)
-                    if eth != nil && bytes.Equal(eth.DstMAC, net.HardwareAddr{0xff, 0xff, 0xff, 0xff, 0xff, 0xff}) {
-                        broadcastPackets++
-                    }
-                }
-            case <-ticker.C:
-                fmt.Printf("每秒广播包数量: %d\n", broadcastPackets)
-                broadcastPackets = 0
-            case <-done:
-                return // 退出 goroutine
-            }
-        }
+        <-signalChan
+        fmt.Println("\n正在退出程序...")
+        close(stop)
     }()
 
-    // 等待信号中断
-    <-sigChan
-    fmt.Println("程序正在退出...")
-    close(done) // 通知 goroutine 停止
-    time.Sleep(1 * time.Second) // 确保 goroutine 有足够时间退出
-    fmt.Println("程序已退出")
+    // 循环读取数据包
+    for {
+        select {
+        case packet := <-packetSource.Packets():
+            broadcastCount++
+            
+            // 解析以太网层
+            ethernetLayer := packet.Layer(layers.LayerTypeEthernet)
+            if ethernetLayer != nil {
+                ethernetPacket, _ := ethernetLayer.(*layers.Ethernet)
+                srcMAC := ethernetPacket.SrcMAC
+
+                // 解析IP层
+                ipLayer := packet.Layer(layers.LayerTypeIPv4)
+                if ipLayer != nil {
+                    ip, _ := ipLayer.(*layers.IPv4)
+                    srcIP := ip.SrcIP
+                    fmt.Printf("捕获到广播包 - 源MAC: %s, 源IP: %s\n", srcMAC, srcIP)
+                } else {
+                    fmt.Printf("捕获到广播包 - 源MAC: %s, 源IP: 未知\n", srcMAC)
+                }
+            }
+            
+            // 每updateInterval打印一次统计信息
+            if time.Since(startTime) >= updateInterval {
+                fmt.Printf("过去 %d 秒内捕获到 %d 个广播包\n", int(updateInterval.Seconds()), broadcastCount)
+                broadcastCount = 0
+                startTime = time.Now()
+            }
+        case <-stop:
+            return
+        }
+    }
+}
+
+// 获取接口的IPv4地址
+func getIPv4(iface pcap.Interface) string {
+    for _, addr := range iface.Addresses {
+        if ipv4 := addr.IP.To4(); ipv4 != nil {
+            return ipv4.String()
+        }
+    }
+    return "无IPv4地址"
 }
