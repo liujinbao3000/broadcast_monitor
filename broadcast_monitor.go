a3f557e6deb69423f6c6ddb1ec93f8eada52d520
amogus <liujinbao3000@163.com>
2024-09-26 22:47:39 +0800

    Update broadcast_monitor.go

--- broadcast_monitor.go
+++ broadcast_monitor.go
@@ -19,7 +19,7 @@ const defaultUpdateInterval = 5 * time.Second
 const version = "2024-09-26" // 更新日期作为版本信息
 
 func main() {
-    interfaceDesc := flag.String("i", "", "指定网络接口的描述（如果有空格，请使用双引号包裹）")
+    interfaceName := flag.String("i", "", "指定网络接口名称")
     updateInterval := flag.Int("f", 5, "监控频率（秒）")
     showHelp := flag.Bool("h", false, "显示帮助信息")
     showVersion := flag.Bool("v", false, "显示版本信息")
@@ -29,7 +29,7 @@ func main() {
 
     if *showHelp {
         fmt.Println("使用说明：")
-        fmt.Println("-i [描述] : 指定网络接口的描述（如果有空格，请使用双引号包裹）")
+        fmt.Println("-i [名称] : 指定网络接口名称")
         fmt.Println("-f [秒]   : 指定监控频率，默认为5秒")
         fmt.Println("-h        : 显示帮助信息")
         fmt.Println("-v        : 显示版本信息")
@@ -55,8 +55,8 @@ func main() {
     interfaces, err := pcap.FindAllDevs()
     checkError(err)
 
-    if *interfaceDesc == "" {
-        fmt.Println("未指定网络接口描述。可用的网络接口:")
+    if *interfaceName == "" {
+        fmt.Println("未指定网络接口名称。可用的网络接口:")
         for i, iface := range interfaces {
             fmt.Printf("%d. 名称: %s, 描述: %s, IPv4: %s\n", i+1, iface.Name, iface.Description, getIPv4(iface))
         }
@@ -67,22 +67,22 @@ func main() {
         if err != nil || choice < 1 || choice > len(interfaces) {
             log.Fatal("无效的选择")
         }
-        *interfaceDesc = interfaces[choice-1].Description
+        *interfaceName = interfaces[choice-1].Name
     }
 
     var selectedInterface *pcap.Interface
     for _, iface := range interfaces {
-        if strings.TrimSpace(iface.Description) == strings.TrimSpace(*interfaceDesc) {
+        if strings.TrimSpace(iface.Name) == strings.TrimSpace(*interfaceName) {
             selectedInterface = &iface
             break
         }
     }
 
     if selectedInterface == nil {
-        log.Fatalf("未找到描述为 '%s' 的网络接口", *interfaceDesc)
+        log.Fatalf("未找到名称为 '%s' 的网络接口", *interfaceName)
     }
 
-    fmt.Printf("选定网络接口: %s - (%s)\n", selectedInterface.Description, getIPv4(*selectedInterface))
+    fmt.Printf("选定网络接口: %s - (%s)\n", selectedInterface.Name, getIPv4(*selectedInterface))
 
     handle, err := pcap.OpenLive(selectedInterface.Name, 1600, true, pcap.BlockForever)
     checkError(err)
@@ -101,47 +101,42 @@ func main() {
 
     signalChan := make(chan os.Signal, 1)
     signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
-    stop := make(chan struct{})
 
     go func() {
         <-signalChan
         fmt.Println("\n正在退出程序...")
-        close(stop)
+        os.Exit(0) // 直接退出
     }()
 
     for {
-        select {
-        case packet := <-packetSource.Packets():
-            broadcastCount++
-            ethernetLayer := packet.Layer(layers.LayerTypeEthernet)
-            ethernetPacket, ok := ethernetLayer.(*layers.Ethernet)
-            if !ok {
-                log.Println("以太网层解析失败")
-                continue
-            }
-            srcMAC := ethernetPacket.SrcMAC.String()
+        packet := <-packetSource.Packets()
+        broadcastCount++
+        ethernetLayer := packet.Layer(layers.LayerTypeEthernet)
+        ethernetPacket, ok := ethernetLayer.(*layers.Ethernet)
+        if !ok {
+            log.Println("以太网层解析失败")
+            continue
+        }
+        srcMAC := ethernetPacket.SrcMAC.String()
 
-            ipLayer := packet.Layer(layers.LayerTypeIPv4)
-            var srcIP string
-            if ipLayer != nil {
-                ip, ok := ipLayer.(*layers.IPv4)
-                if ok {
-                    srcIP = ip.SrcIP.String()
-                } else {
-                    srcIP = "未知"
-                }
+        ipLayer := packet.Layer(layers.LayerTypeIPv4)
+        var srcIP string
+        if ipLayer != nil {
+            ip, ok := ipLayer.(*layers.IPv4)
+            if ok {
+                srcIP = ip.SrcIP.String()
             } else {
                 srcIP = "未知"
             }
-            fmt.Printf("捕获到广播包 - 源MAC: %s, 源IP: %s\n", srcMAC, srcIP)
+        } else {
+            srcIP = "未知"
+        }
+        fmt.Printf("捕获到广播包 - 源MAC: %s, 源IP: %s\n", srcMAC, srcIP)
 
-            if time.Since(startTime) >= time.Duration(*updateInterval)*time.Second {
-                fmt.Printf("过去 %d 秒内捕获到 %d 个广播包\n", *updateInterval, broadcastCount)
-                broadcastCount = 0
-                startTime = time.Now()
-            }
-        case <-stop:
-            return
+        if time.Since(startTime) >= time.Duration(*updateInterval)*time.Second {
+            fmt.Printf("过去 %d 秒内捕获到 %d 个广播包\n", *updateInterval, broadcastCount)
+            broadcastCount = 0
+            startTime = time.Now()
         }
     }
 }
